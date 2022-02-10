f02b80dd1750746fdbc24a2854c39a3ebcf79ac0
Dalton Joaquim Soares Guimarães <dalton.guimaraes500@gmail.com>
2022-02-10 18:25:29 -0300

    Add files via upload

---
+++ Dados_Dalton/Dados_Dalton/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Dados_Dalton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panHistorico = new System.Windows.Forms.Panel();
+            this.nudTipo = new System.Windows.Forms.NumericUpDown();
+            this.nudSoma = new System.Windows.Forms.NumericUpDown();
+            this.cbxOrdem = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NudQuantidade = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pcbDado = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTipo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoma)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudQuantidade)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbDado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(441, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(476, 61);
+            this.panel1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(3, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Histórico";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Dados_Dalton.Properties.Resources.kisspng_reset_button_icon_restart_png_photos5_5a7588a58dcdf3_2872202215176521335808;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(414, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 37);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(169, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Role!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panHistorico
+            // 
+            this.panHistorico.AutoScroll = true;
+            this.panHistorico.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panHistorico.Location = new System.Drawing.Point(441, 69);
+            this.panHistorico.Name = "panHistorico";
+            this.panHistorico.Size = new System.Drawing.Size(476, 485);
+            this.panHistorico.TabIndex = 18;
+            // 
+            // nudTipo
+            // 
+            this.nudTipo.BackColor = System.Drawing.SystemColors.Control;
+            this.nudTipo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nudTipo.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudTipo.Location = new System.Drawing.Point(230, 46);
+            this.nudTipo.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudTipo.Name = "nudTipo";
+            this.nudTipo.Size = new System.Drawing.Size(70, 20);
+            this.nudTipo.TabIndex = 19;
+            this.nudTipo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudTipo.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // nudSoma
+            // 
+            this.nudSoma.BackColor = System.Drawing.SystemColors.Control;
+            this.nudSoma.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nudSoma.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudSoma.Location = new System.Drawing.Point(236, 135);
+            this.nudSoma.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.nudSoma.Name = "nudSoma";
+            this.nudSoma.Size = new System.Drawing.Size(64, 20);
+            this.nudSoma.TabIndex = 21;
+            // 
+            // cbxOrdem
+            // 
+            this.cbxOrdem.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxOrdem.FormattingEnabled = true;
+            this.cbxOrdem.Items.AddRange(new object[] {
+            "Ordem decrescente",
+            "Ordem Natural",
+            "Ordem Crescente"});
+            this.cbxOrdem.Location = new System.Drawing.Point(207, 169);
+            this.cbxOrdem.Name = "cbxOrdem";
+            this.cbxOrdem.Size = new System.Drawing.Size(121, 21);
+            this.cbxOrdem.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 17);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Selecione o dado que vai rolar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(227, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "D";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(12, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 17);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "A quantida de dados rolados:";
+            // 
+            // NudQuantidade
+            // 
+            this.NudQuantidade.BackColor = System.Drawing.SystemColors.Control;
+            this.NudQuantidade.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NudQuantidade.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NudQuantidade.Location = new System.Drawing.Point(236, 92);
+            this.NudQuantidade.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NudQuantidade.Name = "NudQuantidade";
+            this.NudQuantidade.Size = new System.Drawing.Size(64, 20);
+            this.NudQuantidade.TabIndex = 20;
+            this.NudQuantidade.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(12, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 17);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Modificador:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(12, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(193, 17);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Organização dos elementos:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox2.Location = new System.Drawing.Point(432, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(3, 517);
+            this.pictureBox2.TabIndex = 29;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pcbDado
+            // 
+            this.pcbDado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pcbDado.Location = new System.Drawing.Point(116, 238);
+            this.pcbDado.Name = "pcbDado";
+            this.pcbDado.Size = new System.Drawing.Size(184, 151);
+            this.pcbDado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbDado.TabIndex = 23;
+            this.pcbDado.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 566);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pcbDado);
+            this.Controls.Add(this.cbxOrdem);
+            this.Controls.Add(this.nudSoma);
+            this.Controls.Add(this.NudQuantidade);
+            this.Controls.Add(this.nudTipo);
+            this.Controls.Add(this.panHistorico);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dados";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTipo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoma)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudQuantidade)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbDado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panHistorico;
+        private System.Windows.Forms.NumericUpDown nudTipo;
+        private System.Windows.Forms.NumericUpDown nudSoma;
+        private System.Windows.Forms.ComboBox cbxOrdem;
+        private System.Windows.Forms.PictureBox pcbDado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown NudQuantidade;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Dados_Dalton/Dados_Dalton/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dados_Dalton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Numerododado, TodososDados;  
+        int Numerosorteado = 0, Somadosdados;
+        //Declarando variáveis no tipo string e int
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbxOrdem.SelectedIndex = 0;
+            //No evento Load do formulário defino que o número da lista vai ser 0 para começar com ordem decrescente na rolagem de dados
+        }
+
+        List<Panel> Historico = new List<Panel>();
+        //Criei uma variável de lista de paneis, que praticamente é uma variável que possue vários registros nela sendo esses registros paineis e ao contrário de array possue tamanho expansivo
+        private void CriaPanelDado()
+        {
+            //Método criado para criar os controles dentro do painel Resultados que servirão como histórico das rolagens anteriores
+            Panel Resultados = new Panel
+            {
+               BackColor = Color.WhiteSmoke,
+               Width = 456,
+               Height = 95
+            };
+            Historico.Add(Resultados);//criei uma painel Resultados e defini algumas de suas propriedaes e coloquei para ela fazer parte da lista Historico
+            Label Valor = new Label
+            {
+                Location = new Point(16, 34),
+                Font = new Font("Arial Black", 9, FontStyle.Bold),
+                BackColor = Color.Gray,
+                ForeColor = Color.White,
+                Width = 65,
+                Text = Somadosdados.ToString()
+            };
+            Resultados.Controls.Add(Valor);
+            //neste crio um label que vai ficar no panel Resultados, ele portará como texto  a soma de todos os dados rolados mais o modificador que vai ser escolhido
+            Label tipo = new Label
+            {
+                Location = new Point(89, 3),
+                Font = new Font("Arial Black", 8, FontStyle.Bold),
+                ForeColor = Color.Black,
+                Height = 12,
+                Text = Numerododado
+            };
+            Resultados.Controls.Add(tipo);
+            //Esse label mostrará qual dado foi rolado se foi um d20 ou d4 ou outros possíveis
+            PictureBox quadro = new PictureBox
+            {
+                Location = new Point(3, 3),
+                Width = 80,
+                Height = 77,
+                BackColor = Color.Gray
+            };
+            Resultados.Controls.Add(quadro);
+            //uma picture box feita mais por questões de estética 
+            PictureBox linha = new PictureBox
+            {
+                Location = new Point(3, 91),
+                Width = 450,
+                Height = 1,
+                BackColor = Color.Gray
+            };
+            Resultados.Controls.Add(linha);
+            //É uma linha que vai ficar na parte de baixo panel Resultados
+            Label Todosdados = new Label
+            {
+                Location = new Point(89, 18),
+                Font = new Font("Arial Black", 6.25f, FontStyle.Bold),
+                ForeColor = Color.Black,
+                MaximumSize = new Size(370, 0),
+                AutoSize = true,
+                Text = TodososDados
+            };
+            Resultados.Controls.Add(Todosdados);
+            //neste label estará contido todos os dados rolados, se por exemplo rolar dois dados e um tirei 20 e outro 1, será salvo ambas rolagens
+
+            int definirlocal = 1;
+            for (int i = Historico.Count - 1; i >= 0; i--)
+            {
+                if (i != Historico.Count - 1)
+                {
+                    definirlocal += 97;
+                    Historico[i].Location = new Point(0, definirlocal);    
+                }
+                else
+                {
+                    Historico[i].Location = new Point(0, definirlocal);
+                }
+            }
+            panHistorico.Controls.Clear();
+            for (int k = 0; k < Historico.Count; k++)
+            {
+                panHistorico.Controls.Add(Historico[k]);
+            }
+            /*
+              Declarei uma variável do tipo int definir local já começando por 1, o panel resultados que criei vai se juntar a  lista de panels Historico, o primeiro panel Historico
+              vai ficar com o nome Historico[0] e seus sucessivos vão agregando em + 1,no código acima como não tinha definido a localização dos panels Historico, utilizei de uma variável int = 1
+              e uma estrutura de laço de repetição, que dentro dela defini que a variável i vai se repetir até chegar a 0 e i vai ter o valor do número total de Historico -1, o -1 é porque a contagem de todos os paineis Historico
+              partem de 0, dentro desse laço fiz a condição que se a variável i valesse um valor diferente do total de paineis do Historico - 1, somasse a variável definirlocal o valor de 97 que vai definir a localização
+              de cada panel, no else pus que o ultimo registro não precisasse pular 97 pixels começando a partir do pixel 1, Tive que utilizar esta lógica para cada  panel adicionado o ultimo resultado ficasse em primeiro e os feitos anteriormente
+             iam descendo na lista ao lado e para evitar problemas ainda mais complexos eu limpo panHistorico e adiciono novamente os itens dentro dele com outro laço.
+            */
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int dado = Convert.ToInt32(nudTipo.Value) + 1;
+            Random RolaDado = new Random();
+            List<int> NumeroRolado = new List<int>();
+            /* Declarei 3  variáveis, a primeira sendo ela  dado que irá pegar o valor do nudTipo e somará + 1
+             * a varíavel rola dado do tipo random para sortear os números para as rolagens
+             * a ultima novamente uma lista para armazenar todos os dados sorteados.
+             */
+            for (int i = 0; i < NudQuantidade.Value; i++)
+            {
+                Numerosorteado = RolaDado.Next(1, dado);
+                NumeroRolado.Add(Numerosorteado);
+            }
+            /*Este laço vai sortear o número de vezes da quantidade escolhida pelo usuário, vai  sortear um número que seja >= 1 e o tipo do dado e cada número sorteado vai ser
+             * atribuido a uma varíavel numerosorteado que vai ser adcionado na lista Numero rolado
+             */
+            List<int> Organiza = new List<int>();
+            // Esta nova lista foi criada com a finalidade de organiza em ordem decrescente, natural e crescente, todos os numero sorteados.
+
+            switch (cbxOrdem.SelectedIndex)
+            {
+                case 0:
+                    Organiza = NumeroRolado.OrderByDescending(numero => numero).ToList();
+                    break;
+                case 1:
+                    Organiza = NumeroRolado.ToList();
+                    break;
+                case 2:
+                    Organiza = NumeroRolado.OrderBy(numero => numero).ToList();
+                    break;       
+            }
+            // esta switch case vai mudar a ordem conforme o que está selecionado no cbxordem
+
+            for (int k = 0; k < Organiza.Count; k++)
+            {
+                if (k == 0)
+                {
+                    TodososDados = Organiza[k].ToString();
+                    Somadosdados = Organiza[k];
+                }
+                else
+                {
+                    Somadosdados += Organiza[k];
+                    TodososDados += ", " + Organiza[k].ToString();
+                }
+            }
+            /*
+            este for vai ser feito até todos os elementos de organiza estarem finalizados dentro dele para sempre reniciar as varíaveis para novos valores
+            quando k tiver o valor 0 o primeiro valor de organiza vai ser atribuido para duas variáveis, nos valores seguintes na varíavel somadosdados vai somando todos os elementos
+            e a outra varíavel vai escrevendo todos os valores de organiza com , e espaço
+            */
+            Somadosdados += Convert.ToInt32(nudSoma.Value);
+            //essa parte é para adcionar o modificador no somadodados
+
+            if (nudSoma.Value == 0)
+            {
+                Numerododado = NudQuantidade.Value.ToString() + "d" + nudTipo.Value.ToString();
+            }
+            else if (nudSoma.Value > 0)
+            {
+                Numerododado = NudQuantidade.Value.ToString() + "d" + nudTipo.Value.ToString() + "+" + nudSoma.Value.ToString();
+            }
+            else
+            {
+                Numerododado = NudQuantidade.Value.ToString() + "d" + nudTipo.Value.ToString() + nudSoma.Value.ToString(); 
+            }
+            MessageBox.Show(TodososDados + "\r\n\r\n A soma de tudo é " + Somadosdados, Numerododado, MessageBoxButtons.OK);
+            /*
+             criei estes ifs para que a variável string numerorodado receba os textos para cada situação quando o usuários escolhe somar,subtrair e quando nenhum dos dois
+              exibo uma menssage box com todoas as informações das rolagens
+            */
+            CriaPanelDado();
+            //executo o método para salvar no histórico
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panHistorico.Controls.Clear();
+            Historico.Clear();
+            //Botão para apagar o histórico
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (nudTipo.Value)
+            {
+                case 4:
+                    pcbDado.Image = Properties.Resources.d4;
+                    break;
+                case 6:
+                    pcbDado.Image = Properties.Resources.d6;
+                    break;
+                case 8:
+                    pcbDado.Image = Properties.Resources.d8;
+                    break;
+                case 10:
+                    pcbDado.Image = Properties.Resources.d10;
+                    break;
+                case 12:
+                    pcbDado.Image = Properties.Resources.d12;
+                    break;
+                case 20:
+                    pcbDado.Image = Properties.Resources.d20;
+                    break;
+                case 100:
+                    pcbDado.Image = Properties.Resources.d100;
+                    break;
+                default:
+                    pcbDado.Image = Properties.Resources.dado;
+                    break;
+            }
+            //Timer feito para tocar a imagem conforme o tipo de dado   
+        }
+
+    }
+}
